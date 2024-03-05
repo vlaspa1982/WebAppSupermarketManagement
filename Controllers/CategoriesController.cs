1a186f843225fa7edd2e8a27b6fc1fab5b7b3fa8
Vlaspa1982 <v.spalek@seznam.cz>
2024-03-05 07:35:51 +0100

    Add project files.

---
+++ Controllers/CategoriesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            var categories = CategoriesRepository.GetCategories();
+            return View(categories);
+        }
+        public IActionResult Edit(int? id)
+        {
+            var category = CategoriesRepository.GetCategoryById(id.HasValue?id.Value:0);
+            return View(category);
+        }
+        
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            CategoriesRepository.UpdateCategory(category.CategoryId, category);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+
+
+var app = builder.Build();
+app.UseRouting();
+app.UseStaticFiles();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=home}/{action=index}/{id?}");
+
+app.Run();
